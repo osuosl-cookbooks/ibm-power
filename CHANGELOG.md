9b1738800c70473098f15de00b35be35cc48f8e8
Lance Albertson <lance@osuosl.org>
2016-10-03 09:26:45 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+ibm-power CHANGELOG
+===================
+This file is used to list changes made in each version of the
+ibm-power cookbook.
+
+0.1.0
+-----
+- Initial release of ibm-power
+
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'ibm-power'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apachev2'
+issues_url       'https://github.com/osuosl-cookbooks/ibm-power/issues'
+source_url       'https://github.com/osuosl-cookbooks/ibm-power'
+description      'Installs/Configures ibm-power'
+long_description 'Installs/Configures ibm-power'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
