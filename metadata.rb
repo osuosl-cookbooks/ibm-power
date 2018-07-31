7570fc1c066652f99a0bc6eaf124216ec3fe3231
Jason Chen <jasonc@osuosl.org>
2018-07-31 14:50:08 -0700

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'ibm-power'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
-license          'apachev2'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
 issues_url       'https://github.com/osuosl-cookbooks/ibm-power/issues'
 source_url       'https://github.com/osuosl-cookbooks/ibm-power'
 description      'Installs/Configures ibm-power'
