542cd966f53adbcbd2466d5eff54f241a938c88b
Catherine Stevens <catherine@crimson.workstation.cass.oregonstate.edu>
2019-04-23 11:12:06 -0700

    cookstyle fix

--- test/integration/ohai_plugin/inspec/ohai_plugin_spec.rb
+++ test/integration/ohai_plugin/inspec/ohai_plugin_spec.rb
@@ -3,7 +3,7 @@ plugin_directory = '/tmp/kitchen/ohai/plugins'
 arch = os.arch
 
 describe bash("ohai -d #{plugin_directory} ibm_power") do
-  its('exit_status') { should eq 0 }  
+  its('exit_status') { should eq 0 }
   if arch == 'ppc64le'
     its('stdout') { should match /cpu_model.*power/ }
   else
