7d349a44ab57372678b5bf10eeda6d7d9902ec15
Catherine Stevens <catherine@crimson.workstation.cass.oregonstate.edu>
2019-04-23 11:10:13 -0700

    Called OS resource using a variable to circumvent InSpec complaining about its placement

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,5 +1,7 @@
+arch = os.arch
+
 describe yum.repo('ibm-power-tools') do
-  if os.arch == 'ppc64le'
+  if arch == 'ppc64le'
     it { should exist }
     it { should be_enabled }
   else
@@ -8,21 +10,9 @@ describe yum.repo('ibm-power-tools') do
   end
 end
 
-#if os.arch == 'ppc64le'
-#  describe yum.repo('ibm-poower-tools') do
-#    it { should exist }
-#    it { should be_enabled }
-#  end
-#else 
-#  describe yum.repo('ibm-poower-tools') do
-#    it { should_not exist }
-#    it { should_not be_enabled }
-#  end
-#end
-
 %w(ppc64-diag powerpc-utils).each do |p|
   describe package(p) do
-    if os.arch == 'ppc64le'
+    if arch == 'ppc64le'
       it { should be_installed }
     else
       it { should_not be_installed }
--- test/integration/ohai_plugin/inspec/ohai_plugin_spec.rb
+++ test/integration/ohai_plugin/inspec/ohai_plugin_spec.rb
@@ -1,10 +1,12 @@
 plugin_directory = '/tmp/kitchen/ohai/plugins'
 
+arch = os.arch
+
 describe bash("ohai -d #{plugin_directory} ibm_power") do
-  its('exit_status') { should eq 0 }
-  if os.arch == 'ppc64le'
-    its('stdout') { should match(/cpu_model.*power/) }
+  its('exit_status') { should eq 0 }  
+  if arch == 'ppc64le'
+    its('stdout') { should match /cpu_model.*power/ }
   else
-    its('stdout') { should_not match(/cpu_model.*power/) }
+    its('stdout') { should_not match /cpu_model.*power/ }
   end
 end
